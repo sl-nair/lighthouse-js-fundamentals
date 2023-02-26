130e02e0cbdc5de4b6d3d8008558d2595dc2bd7c
Sethu Nair <sethunair2102@gmail.com>
2023-02-26 11:46:46 -0700

    Added new example

--- conditionals.js
+++ conditionals.js
@@ -22,3 +22,33 @@ let raining = true;
 if (!raining) {
   console.log("Leave your umbrella at home");
 }
+
+
+const whichSchool  = function (age) {
+  // Your code in here ...
+}
+
+let x = 60
+while (x >= 0) {
+    if (x === 50){
+        console.log("Orbiter transfers from ground to internal power");
+        x = x-1;
+    } else if (x === 31){
+        console.log("Ground launch sequencer is go for auto sequence start");
+        x = x-1;
+    } else if (x === 16){
+        console.log("Ground launch sequencer is go for auto sequence start");
+        x = x-1;
+    } else if (x === 10){
+        console.log("Activate main engine hydrogen burnoff system");
+        x = x-1;
+    } else if (x === 6){
+        console.log("Main engine start");
+        x = x-1;
+    } else if (x === 0){
+        console.log("Solid rocket booster ignition and liftoff!");
+        x = x-1;
+    } else {
+        console.log("T-"+x+"seconds")
+    }
+}
